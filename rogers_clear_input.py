d9627f58b96c6071dbb45d92985cd6761f3f3421
Craig Huckson <craig.huckson@gmail.com>
2022-04-10 15:13:21 -0400

    finished python program for rogers clears

--- rogers_clear_input.py
+++ rogers_clear_input.py
@@ -1,21 +1,38 @@
-#routine to take in all info
+# routine to take in all info
+import pyinputplus as pyip
+
+
 totalpages = 0
 
+
 def clear_form():
-    north = input('Enter north boundary:')
-    south = input('Enter south boundary:')
-    west = input('Enter west boundary:')
-    east = input('Enter east boundary:')
-    print('Enter clear reason\n1=Regular\n2=FTTH')
-    clear_reason = input('3 = FO only\n:')
+    form = {}
+    form["north"] = pyip.inputStr("Enter north boundary:")
+    form["south"] = pyip.inputStr("Enter south boundary:")
+    form["west"] = pyip.inputStr("Enter west boundary:")
+    form["east"] = pyip.inputStr("Enter east boundary:")
+    form["clear_reason"] = pyip.inputMenu(
+        ["regular", "ftth", "foonly"],
+        numbered=True,
+        prompt="Enter the reason for the clear\n",
+    )
+    return form
 
-ticket_number = input('Enter ticket number:')
+def write_parse_file(form,ticket_number,page_number,total_pages):
+    
+    with open(f'{ticket_number}-{page_number}.txt',mode='a') as tf:
+        tf.write(f'{page_number}\n')
+        tf.write(f'{total_pages}\n')
+        for _ in form.values():
+            tf.write(f'{_}\n')
 
-while True:
-    totalpages = input('Enter number of pages:')
-    if int(totalpages) > 1:
-        break
+
+ticket_number = pyip.inputNum("Enter ticket number:")
+
+totalpages = pyip.inputNum("Enter number of pages:", min=1)
 
 if totalpages == 1:
-    units = '1C'
-    clear_form()
+    units = "1C"
+    page_number = 1
+    form = clear_form()
+    write_parse_file(form,ticket_number,page_number,totalpages)
